307a1e0bf0ca9549f75137af0721ab64cb5db30c
adgvcxz <andzhaov@gmail.com>
2021-10-13 18:51:11 +0800

    init project

---
+++ app/src/main/java/com/adgvcxz/stateflow/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.adgvcxz.stateflow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
